e7c8666ab75f3c06ada853deaac5dc72f351da11
ChrisKye <70194904+ChrisKye@users.noreply.github.com>
2022-11-27 18:08:28 -0500

    Update master.py

--- code/master.py
+++ code/master.py
@@ -8,7 +8,7 @@ This script downloads all 10-K/10-Q filings for a list of gvkeys
 """
 
 import pandas as pd
-from sec_api import QueryApi, FullTextSearchApi
+from sec_api import FullTextSearchApi
 import pyreadstat
 import time
 
@@ -16,24 +16,20 @@ start_time = time.time()
 
 ##Load in gvkey List
 gv_list, meta = pyreadstat.read_dta('../data/gvkey_list.dta')
-gv_list.head()
 
 ## Sort list & change to int
 gv_list = gv_list.sort_values(by = 'gvkey')
 gv_list = gv_list.drop(19)
 gv_list['gvkey'] = gv_list['gvkey'].astype(str).astype(int)
-gv_list.head()
 
 ##Load in reference map
 cikmap = pd.read_csv('../data/index.csv')
 cikmap.set_index(keys = cikmap['gvkey'], inplace=True) ##gvkeys as indices
 cikmap['cik']=cikmap['cik'].fillna(-1)
 cikmap['cik'] = cikmap['cik'].astype(int)
-cikmap.head(10)
 
 ##Download all filings
 master_df = pd.DataFrame(columns=['CIK','Company_Name','Filing','Date'])
-master_df.head()
 
 ## Create list to track unmatched gvkeys
 missingcik = []
@@ -42,8 +38,7 @@ random_error = []
 nomatch = []
 many_filings = []
 
-queryApi = QueryApi(api_key="924e625c185d49e08371c3d2291c83ec2f9403289cd73d576da7071a693b147d")
-queryApi_2 = FullTextSearchApi(api_key = "924e625c185d49e08371c3d2291c83ec2f9403289cd73d576da7071a693b147d")
+queryApi = FullTextSearchApi(api_key = "924e625c185d49e08371c3d2291c83ec2f9403289cd73d576da7071a693b147d")
 
 for index in range(50):
     ##Find first company cikmap
@@ -76,18 +71,6 @@ for index in range(50):
             continue
         
         print("CIK found: " + str(cikk))
-        ## Run first query
-        # query = {
-        #     "query":  {
-        #         "query_string": {
-        #             "query": "cik:" + str(cikk) + " AND (formType:\"10-K\" OR \"10-Q\")"
-        #         }
-        #     },
-        #     "from": "0",
-        #     "sort": [{"filedAt": {
-        #                 "order": "asc"
-        #             }}]
-        # }
         
         query = {
             "query": "",
@@ -97,7 +80,7 @@ for index in range(50):
             "endDate": "2022-11-30"
         }
         
-        filings = queryApi_2.get_filings(query)
+        filings = queryApi.get_filings(query)
         
         ##check for random error
         if (len(filings['total']) == 0):    
@@ -170,5 +153,5 @@ master_df['Date'] = pd.to_datetime(master_df['Date'])
 master_df['Date'] = master_df['Date'].dt.date
 master_df['CIK'] = pd.to_numeric(master_df['CIK'])
 
-master_df.to_csv("../data/master_v3.csv")
+master_df.to_csv("../data/master.csv")
 print("Complete.")
